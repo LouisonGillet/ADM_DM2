294b275a9ec270027806c7f59824c3b24ac135c2
Louison Gillet <louison.gillet@etu.umontpellier.fr>
2024-11-23 18:08:13 +0100

    création script.R

---
+++ R/script.R
@@ -0,0 +1,9 @@
+# En tête ---- 
+
+#Desc : DM2 ADM
+#Date: 23/11/2024
+#Auteur : EL MAZZOUJI Wahel & GILLET Louison 
+
+# Dataframe ---- 
+Datagenus <- read.csv("data/Datagenus.csv", sep=";")
+datapeuple <- Datagenus[1:1000,] # On ne prend pas la ligne 1001 
