fd49ddc6501bfddf0a3459a4e2c5591938fbe69d
Louison Gillet <louison.gillet@etu.umontpellier.fr>
2024-11-28 11:22:22 +0100

    Premieres rédactions

--- R/script.R
+++ R/script.R
@@ -52,43 +52,55 @@ datapeuple <- as.data.frame(densite_centree_reduite)
 dp <- dist(datapeuple, method = "euclidean")  # Matrice des distances
 
 # CAH avec Ward ----
-CAHDP <- hclust(d = dp, method = "ward.D")  # CAH avec la méthode de Ward
-plot(CAHDP,labels=FALSE)  # Dendrogramme de la hiérarchie de la CAH
+CAHWard <- hclust(d = dp, method = "ward.D")  # CAH avec la méthode de Ward
+CAHMax <- hclust(d = dp, method = "complete")  # CAH avec la méthode de Max
+CAHMoy <- hclust(d = dp, method = "mcquitty")  # CAH avec la méthode de Moy
+CAHMin <- hclust(d = dp, method = "single")  # CAH avec la méthode de Min 
 
-# Calcul des R2 pour chaque variable ----
-R2_PDP4 = cbind(rep(0 , ncol(datapeuple)))
-for (i in cbind(1:ncol(datapeuple))) {
-  R2_PDP4[i] = summary(lm(datapeuple[,i]~as.factor(PDP4)))$r.squared
-}
-row.names(R2_PDP4) = colnames(datapeuple)
+plot(CAHWard, labels = FALSE, main = "Dendrogramme - Méthode de Ward")
+plot(CAHMax, labels = FALSE, main = "Dendrogramme - Méthode Complete (Max)")
+plot(CAHMoy, labels = FALSE, main = "Dendrogramme - Méthode McQuitty (Moy)")
+plot(CAHMin, labels = FALSE, main = "Dendrogramme - Méthode Single (Min)")
 
-# Calcul du R2 global ----
-R2G_PV4 = mean(R2_PDP4)
-
-# Résultats finaux ----
-print("R2 global pour la partition en 4 classes :")
-print(R2G_PV4)
-
-# CAH avec Max ----
-CAHDP2 <- hclust(d = dp, method = "complete")  # CAH avec la méthode de Max
-plot(CAHDP2,labels=FALSE)  # Dendrogramme de la hiérarchie de la CAH
-
-# CAH avec moy ----
-CAHDP3 <- hclust(d = dp, method = "mcquitty")  # CAH avec la méthode de Max
-
-plot(CAHDP3,labels=FALSE) # Dendrogramme de la hiérarchie de la CAH
+#### Nombre de classes #### 
+k <- 8
 
 # Partition en k=4 classes ----
-PWard = cutree(tree = CAHDP, k=4) #Ward
-PMax = cutree(tree = CAHDP2, k=4) #Sautmax
-PMoy = cutree(tree = CAHDP3, k=4) #Sautmoy
+PWard = cutree(tree = CAHWard, k) #Ward
+PMax = cutree(tree = CAHMax, k) #Sautmax
+PMoy = cutree(tree = CAHMoy, k) #Sautmoy
+PMin = cutree(tree = CAHMin, k) #Sautmin
+
+# Calcul des R2 pour chaque variable ----
+R2_PWard = cbind(rep(0 , ncol(datapeuple)))
+R2_PMax = cbind(rep(0 , ncol(datapeuple)))
+R2_PMoy = cbind(rep(0 , ncol(datapeuple)))
+R2_PMin = cbind(rep(0 , ncol(datapeuple)))
+
+for (i in 1:ncol(datapeuple)) {
+  R2_PWard[i] = summary(lm(datapeuple[,i] ~ as.factor(PWard)))$r.squared
+  R2_PMax[i] = summary(lm(datapeuple[,i] ~ as.factor(PMax)))$r.squared
+  R2_PMoy[i] = summary(lm(datapeuple[,i] ~ as.factor(PMoy)))$r.squared
+  R2_PMin[i] = summary(lm(datapeuple[,i] ~ as.factor(PMin)))$r.squared
+}
+
+row.names(R2_PWard) = colnames(datapeuple)
+row.names(R2_PMax) = colnames(datapeuple)
+row.names(R2_PMoy) = colnames(datapeuple)
+row.names(R2_PMin) = colnames(datapeuple)
+
+# Calcul du R2 global ----
+R2G_PWard = mean(R2_PWard)
+R2G_PMax = mean(R2_PMax)
+R2G_PMoy = mean(R2_PMoy)
+R2G_PMin = mean(R2_PMin)
 
 # Fonction pour calculer l'indice de Rand
 rand_index <- function(partition1, partition2) {
   contingency_table <- table(partition1, partition2)
   n <- sum(contingency_table)
   total_pairs <- choose(n, 2)
-  
+
   same_cluster_pairs <- sum(choose(contingency_table, 2))
   
   different_cluster_pairs <- total_pairs - sum(apply(contingency_table, 1, sum)^2) / 2 -
@@ -101,7 +113,28 @@ rand_index <- function(partition1, partition2) {
   return(rand_index)
 }
 
-indice_de_rand <- rand_index(PWard, PMax)
-cat("Indice de Rand :", indice_de_rand, "\n")
+# Indices de rand 
+rand_Ward_Max <- rand_index(PWard, PMax)
+rand_Ward_Moy <- rand_index(PWard, PMoy)
+rand_Ward_Min <- rand_index(PWard,PMin)
+rand_Max_Moy <- rand_index(PMax, PMoy)
+rand_Max_Min <- rand_index(PMax,PMin)
+rand_Moy_Min <- rand_index(PMoy,PMin)
 
+# Création des matrices d'indicatrices pour chaque partition ----
+ICWard = data.frame(model.matrix(~as.factor(PWard) - 1))
+ICMax = data.frame(model.matrix(~as.factor(PMax) - 1))
+ICMoy = data.frame(model.matrix(~as.factor(PMoy) - 1))
+ICMin = data.frame(model.matrix(~as.factor(PMin) - 1))
+
+mICWard = as.matrix(ICWard)
+mDP = as.matrix(datapeuple)
+CentresC2 = solve(t(mICWard) %*% mICWard) %*% t(mICWard)%*% mDP
+KMDP2 = kmeans(datapeuple, CentresC2)
+KMDP2$cluster
+boxplot(datapeuple[,27]~as.factor(PWard))
+boxplot(datapeuple[,27]~as.factor(KMDP2$cluster))
+
+# Sauvegarde pour Rmd ---- 
+save.image(file = "ressources/prepa.RData")
 
